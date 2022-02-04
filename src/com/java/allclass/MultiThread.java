b4de08e2f1beaf8cacf168783d6833d4f64a84f0
Venkatesh <viv7012@gmail.com>
2022-02-04 15:42:06 +0530

    Set the Name of the Thread and Set the Priority of thread.

--- src/com/java/allclass/MultiThread.java
+++ src/com/java/allclass/MultiThread.java
@@ -2,6 +2,10 @@ package com.java.allclass;
 
 public class MultiThread {
 
+	static Thread th1;
+	static Thread th2;
+	static Thread th3;
+	
 	static {
 		System.out.println("Hello i am from Static..");
 	}
@@ -27,6 +31,43 @@ public class MultiThread {
 			System.out.println(i + " * "+ num + " = "+ i*num);
 			Thread.sleep(1000);
 		}
+	}
+
+	public static void threadName() {
+		
+		th1 = new Thread(new Runnable(){
+			@Override
+			public void run() {
+				System.out.println("Venkat 1");
+			}
+		}); 
+		th2 = new Thread(new Runnable(){
+			@Override
+			public void run() {
+				System.out.println("Vicky 2");
+			}
+		});
+		th3 = new Thread(new Runnable(){
+			@Override
+			public void run() {
+				System.out.println("Vj 3");
+			}
+		});
+		th1.setName("Venkat");
+		th1.setPriority(10);
+		th2.setName("Vicky");
+		th2.setPriority(10);
+		th3.setName("Vj");
+		th3.setPriority(10);
+		System.out.println(th1.getName() + "  "+th1.getPriority());
+		System.out.println(th2.getName()+"  "+th2.getPriority());
+		System.out.println(th3.getName()+"  "+th3.getPriority());
+		
+		th1.start();
+		th3.start();
+		th2.start();
+		
+		
 	}
 	
 	public static void main(String[] args) throws Throwable {
@@ -59,13 +100,18 @@ public class MultiThread {
 			}	
 		});
 		
-		th.start();
+//		th.start();
 //		System.out.println("Thread ID: "+th.getId());
 //		System.out.println("Thread Name: "+th.getName());
-		th1.start();
+//		th1.start();
 //		System.out.println("Thread 1 ID: "+th1.getId());
 //		System.out.println("Thread 1 Name: "+th1.getName());
 //		System.out.println("Hello");
+		
+		
+		threadName();
+		
+		
 	}
 
 }
